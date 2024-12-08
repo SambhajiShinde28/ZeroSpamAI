f4cffa61c357a8804d05dc8eda767ca967b0417a
SambhajiShinde28 <sambhajisshinde800@gmail.com>
2024-12-08 11:25:17 +0530

    Added MySQL database to project.

--- zerospamai_backend/zerospamai_backend/settings.py
+++ zerospamai_backend/zerospamai_backend/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'zerospamai_backend.wsgi.application'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+	'default': {
+		'ENGINE': 'django.db.backends.mysql',
+		'NAME': 'zerospamai_database',
+		'USER': 'root',
+		'PASSWORD': 'Sam#26',
+		'HOST':'localhost',
+		'PORT':'3306',
+	}
 }
 
 
