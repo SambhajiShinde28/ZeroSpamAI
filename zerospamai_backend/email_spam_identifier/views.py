bc4e05e77f699819b9b55c5d77c6b3c36a8b6c25
SambhajiShinde28 <sambhajisshinde800@gmail.com>
2024-12-08 13:35:40 +0530

    Added complete backend code to project.

--- zerospamai_backend/email_spam_identifier/views.py
+++ zerospamai_backend/email_spam_identifier/views.py
@@ -1,6 +1,50 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
+from .models import EmailSpamModel
+
+import pandas as pd
+import joblib
+import numpy as np
+import os
+from django.conf import settings
+import sklearn
 
 
+model_path=os.path.join(settings.BASE_DIR,'ml-model/EmailSpam_Model.pkl')
+vectorizer_path=os.path.join(settings.BASE_DIR,'ml-model/Message_Vectorizer.pkl')
+
+MultinomialNB_model = joblib.load(model_path)
+vectorize = joblib.load(vectorizer_path)
+
+
+@csrf_exempt
 def SpamPredict(req):
-    return HttpResponse("This is Email Classifier.")
+    
+    if req.method == "POST":
+
+        data=json.load(req)
+
+        if data["email"] != "":
+             
+            encoaded_data=vectorize.transform([data["email"]])
+            model_prediction=MultinomialNB_model.predict(encoaded_data)
+            model_probability=MultinomialNB_model.predict_proba(encoaded_data)
+
+            Spam_Percentage=round(model_probability[0][0]*100,2)
+            NotSpam_Percentage=round(model_probability[0][1]*100,2)
+
+            Category="Spam" if model_prediction[0]==1 else "NotSpam"
+
+            savingData = EmailSpamModel(UserMessage=data["email"], SpamProbability=Spam_Percentage, NotSpamProbability=NotSpam_Percentage, ModelPrediction=model_prediction[0], Category=Category)
+            savingData.save()
+            
+            return JsonResponse({"Message":"prediction","Category":Category,"SpamPercentage":Spam_Percentage,"NotSpamPercentage":NotSpam_Percentage})
+        
+        else:
+            return JsonResponse({"Message":"EmailEmpty"})
+    
+    else:
+        return JsonResponse({"Message":"wrong request"})
+    
--- zerospamai_backend/zerospamai_backend/settings.py
+++ zerospamai_backend/zerospamai_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'email_spam_identifier',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -48,6 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
 
 ROOT_URLCONF = 'zerospamai_backend.urls'
@@ -126,3 +128,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+
+CORS_ALLOWED_ORIGINS = [
+    'http://localhost:3000',
+]
